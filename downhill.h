4ae4398c5be2610af2940c07df0f906cbe60ca16
Marc Volker Dickmann <marc-q@users.noreply.github.com>
2017-03-07 16:04:07 +0100

    Update downhill.h

--- downhill.h
+++ downhill.h
@@ -1,18 +1,25 @@
 #ifndef __DOWNHILL_H__
 #define __DOWNHILL_H__
 
-enum close_tags
+#define TAG_MAX 20
+#define TAG_MASK_VALUE 0x7FFFFFFF
+#define TAG_MASK_CLOSE 0x80000000
+
+// The last bit is reserved for the open/close flag!
+enum _tags
 {
-	CLOSE_TAG_NONE = 0,
-	CLOSE_TAG_H1,
-	CLOSE_TAG_H2,
-	CLOSE_TAG_H3,
-	CLOSE_TAG_H4,
-	CLOSE_TAG_H5,
-	CLOSE_TAG_H6,
-	CLOSE_TAG_BLOCKQUOTE,
-	CLOSE_TAG_UL,
-	CLOSE_TAG_LI
+	TAG_NONE = 0,
+	
+	TAG_H1,
+	TAG_H2,
+	TAG_H3,
+	TAG_H4,
+	TAG_H5,
+	TAG_H6,
+	
+	TAG_BLOCKQUOTE,
+	TAG_UL,
+	TAG_LI
 };
 
 struct _parser
@@ -20,7 +27,8 @@ struct _parser
 	size_t list;
 	bool emphasis;
 	bool skip_line;
-	enum close_tags close_tag[3];
+	
+	enum _tags tag[TAG_MAX];
 	
 	size_t buf_pos;
 	size_t buf_len;
